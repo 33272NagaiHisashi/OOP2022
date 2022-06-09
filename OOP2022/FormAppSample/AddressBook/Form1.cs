d723ad83ac16ada8dfa7d027db09bcba6a72ca1a
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-06-09 14:15:46 +0900

    新規AddressBook

--- OOP2022/FormAppSample/AddressBook/Form1.cs
+++ OOP2022/FormAppSample/AddressBook/Form1.cs
@@ -8,18 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Sample0607 {
+namespace AddressBook {
 	public partial class Form1 : Form {
-
-		Random rand = new Random();
 		public Form1() {
 			InitializeComponent();
-		}
-
-		private void button1_Click_1(object sender, EventArgs e) {
-			var min = int.Parse(tbMin.Text);
-			var max = int.Parse(tbMax.Text);
-			tbRandom.Text = rand.Next((int)min,(int)max + 1).ToString();
 		}
 	}
 }
