793f31cd43679ab0078a815e9d61d2f9ae1522ad
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-06-14 14:20:15 +0900

    統一試験

--- OOP2022/Test202206/TestProject/Test01/ScoreCounter.cs
+++ OOP2022/Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,45 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);   
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> student = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                Student sale = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
+        
+
+
+
+
+
+
+
+    }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            var dict = new Dictionary<string,int>();
+            foreach (var stud in _score) {
+                if (dict.ContainsKey(stud.Name))
+                    dict[stud.Name] += stud.Score;
+                else
+                    dict[stud.Name] = stud.Score;
+            }
+            return dict;
         }
     }
 }
