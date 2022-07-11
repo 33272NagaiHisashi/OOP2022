fa67177ae6a40176ae202d2cabd1610b91613bb7
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-07-11 11:11:15 +0900

    Exercise12.1 完成

--- OOP2022/Chapter12/Exercise01/EmployeeJ.cs
+++ OOP2022/Chapter12/Exercise01/EmployeeJ.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Exercise01 {
-	public class Employee {
+	[DataContract(Name = "EmployeeJ")]
+	class EmployeeJ {
 		public int Id { get; set; }
+		[DataMember(Name = "name")]
 		public string Name { get; set; }
+		[DataMember(Name = "HireDate")]
 		public DateTime HireDate { get; set; }
 		public override string ToString() {
 			return string.Format("[Id={0},Name={1},HireDate={2}]", Id, Name, HireDate);
