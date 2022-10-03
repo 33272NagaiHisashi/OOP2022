c9a871e3b0ddc017b88920457f4a470504088975
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-10-03 16:00:14 +0900

    20221003

--- OOP2022/Chapter15/Chapter15/Program.cs
+++ OOP2022/Chapter15/Chapter15/Program.cs
@@ -7,12 +7,48 @@ using System.Threading.Tasks;
 namespace Chapter15 {
 	class Program {
 		static void Main(string[] args) {
-			var price = Library.Books
-				.OrderBy(b=>b.CategoryId)
-				.ThenByDescending(b=>b.PublishedYear);
-			foreach (var p in price) {
-				Console.WriteLine(p);
+			var years = new List<int> { };
+			Console.WriteLine("出力したい西暦を入力(終了：-1)");
+			while (true) {
+				years.Add(int.Parse(Console.ReadLine()));
+				if (years.Contains(-1)) break;
 			}
+			Console.WriteLine();
+			Console.Write("昇順：１ 降順：２　:");
+			var num = int.Parse(Console.ReadLine());
+			if (num == 1) {
+				var books = Library.Books.Where(b => years.Contains(b.PublishedYear))
+										 .OrderBy(b => b.PublishedYear);
+				var groups = Library.Books.GroupBy(b => b.PublishedYear)
+										  .OrderBy(g => g.Key);
+				foreach (var b in books) {
+					Console.WriteLine(b);
+					Console.WriteLine();
+				}
+				foreach (var g in groups) {
+					Console.WriteLine($"{g.Key}年");
+					foreach(var b in books) {
+						Console.WriteLine("	{0} {1} {2}",b.Title,b.CategoryId,b.Price);
+					}
+				}
+			}
+			if (num == 2) {
+				var books = Library.Books.Where(b => years.Contains(b.PublishedYear))
+										 .OrderByDescending(b => b.PublishedYear);
+				var groups = Library.Books.GroupBy(b => b.PublishedYear)
+										  .OrderByDescending(g => g.Key);
+				foreach (var b in books) {
+					Console.WriteLine(b);
+					Console.WriteLine();
+				}
+				foreach (var g in groups) {
+					Console.WriteLine($"{g.Key}年");
+					foreach (var b in books) {
+						Console.WriteLine("	{0} {1} {2}", b.Title, b.CategoryId, b.Price);
+					}
+				}
+			} 
 		}
 	}
 }
+
