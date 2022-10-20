308d2540432abb3a3a5412b91e88dead4b6b36c7
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-10-20 11:22:45 +0900

    SampleSlider

--- OOP2022/WPF/NumberGame/MainWindow.xaml.cs
+++ OOP2022/WPF/NumberGame/MainWindow.xaml.cs
@@ -18,19 +18,34 @@ namespace NumberGame {
 	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		Random rnd = new Random();
+		private int num;
+
 		public MainWindow() {
 			InitializeComponent();
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e) {
 			Button bt = (Button)sender;
-			Random rnd = new Random();
-			var num = rnd.Next(1) + 1;
-			if ((object)num == bt.Content) {
-				MessageBox.Show("正解！");
-			} else {
-				MessageBox.Show("はずれ");
+			if (num == int.Parse(bt.Content.ToString())) {
+				bt.Background = Brushes.Green;
+				infoDisp.Text = "正解！";
 			}
+			if (num > int.Parse(bt.Content.ToString())) {
+				bt.Background = Brushes.Red;
+				infoDisp.Text = int.Parse(bt.Content.ToString()) + " より大きい";
+			}
+			if (num < int.Parse(bt.Content.ToString())) {
+				bt.Background = Brushes.Red;
+				infoDisp.Text = int.Parse(bt.Content.ToString()) + " より小さい";
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			infoDisp.Text = "ゲームスタート";
+			num = rnd.Next(25) + 1;
+			this.Title = num.ToString();
 		}
 	}
 }
