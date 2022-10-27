a56ce6150011ea196addd59a33346163736314d7
33272NagaiHisashi <104185099+33272NagaiHisashi@users.noreply.github.com>
2022-10-27 15:57:13 +0900

    20221027

--- OOP2022/WPF/ColorChecker/MainWindow.xaml.cs
+++ OOP2022/WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,7 +20,59 @@ namespace ColorChecker {
 	/// </summary>
 	public partial class MainWindow : Window {
 		public MainWindow() {
+			List<MyColor> stockMyColors = new List<MyColor>();
 			InitializeComponent();
+			DataContext = GetColorList();
+		}
+		private MyColor[] GetColorList() {
+			return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+				.Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e) {
+			setColor();
+		}
+		private void Slider_ValueChanged(Object sender,RoutedPropertyChangedEventArgs<double> e) {
+			setColor();
+		}
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+			var color = mycolor.Color;
+			var name = mycolor.Name;
+
+			SolidColorBrush scb = new SolidColorBrush(color);
+			lColor.Background = scb;
+
+			tbRed.Text = Convert.ToString(color.R, 10);
+			tbGreen.Text = Convert.ToString(color.G, 10);
+			tbBlue.Text = Convert.ToString(color.B, 10);
+			setColor();
+		}
+
+		private void setColor() {
+			var R = byte.Parse(tbRed.Text);
+			var G = byte.Parse(tbGreen.Text);
+			var B = byte.Parse(tbBlue.Text);
+
+			SolidColorBrush scb = new SolidColorBrush(Color.FromRgb(R,G,B));
+			lColor.Background = scb;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
 		}
 	}
+	/// <summary>
+	/// 色と色名を保持するクラス
+	/// </summary>
+	public class MyColor {
+		public Color Color { get; set; }
+		public string Name { get; set; }
+	}
 }
+
